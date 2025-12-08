d799671bb4fe7eba4ea490d1c12fbf74a4c10322
sizoff <sizoff.dev@yandex.ru>
2025-12-08 13:57:19 -0500

    fixed too big number for compression is set

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::env;
 use server::{Server, invokers_side::InvokersSide, testing_system_side::TestingSystemSide, control_panel::ControlPanel};
 
 pub const MAX_MESSAGE_SIZE: usize = 1 << 31;
-pub const COMPRESSION_LEVEL: u32 = 64 * 1024 * 1024;
+pub const COMPRESSION_LEVEL: u32 = 9;
 
 #[tokio::main]
 async fn main() {
